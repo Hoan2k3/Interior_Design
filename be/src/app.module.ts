7d85ff89aca9bfda8de8a7a4f6809b13668d35f4
hoanTran <hoanspurs2@gmail.com>
2026-04-06 21:53:36 +0700

    Connect db success

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('connection_string')
+      })
+    })
+    
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
